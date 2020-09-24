d63ed15804d641332c1b45e4b90c2fb58b531641
firas-hamdi <hamdi.firas.1994@ieee.org>
2020-09-24 13:00:26 +0200

    Add files

---
+++ ses/Output/secure_bootloader_ble_s140_pca10056 Release/Obj/oberon_backend_ecc.d
@@ -0,0 +1,4 @@
+Output/secure_bootloader_ble_s140_pca10056\ Release/Obj/oberon_backend_ecc.o: \
+ C:\Users\hp\Desktop\bootloader_sdk\components\libraries\crypto\backend\oberon\oberon_backend_ecc.c \
+ ../config/sdk_config.h \
+ ../../../../../components/libraries/util/nordic_common.h
---
+++ ses/Output/secure_bootloader_ble_s140_pca10056 Release/Obj/oberon_backend_ecdh.d
@@ -0,0 +1,4 @@
+Output/secure_bootloader_ble_s140_pca10056\ Release/Obj/oberon_backend_ecdh.o: \
+ C:\Users\hp\Desktop\bootloader_sdk\components\libraries\crypto\backend\oberon\oberon_backend_ecdh.c \
+ ../config/sdk_config.h \
+ ../../../../../components/libraries/util/nordic_common.h
